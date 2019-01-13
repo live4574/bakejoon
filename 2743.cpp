396df7e85cb00d82509907e845414c8d1cbe43ac
live4574 <adolfd@naver.com>
2019-01-13 17:26:38 +0900

    19.01.13

---
+++ 2743.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <stack>
+#include<string.h>
+using namespace std;
+int main() {
+	char str[101];
+	cin >> str;
+	cout << strlen(str)<<endl;
+	//system("pause");
+	return 0;
+}
